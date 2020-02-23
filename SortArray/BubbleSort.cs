85994563e58bdac7d17ead613cade5c2b3e29c64
Kokojam7a <Kokojam7a@DESKTOP-2JBLTON>
2020-02-23 20:32:43 +0200

    111

---
+++ SortArray/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArray
+{
+    class BubbleSort
+    {
+        int[] a = new int[20];
+        public void GenerateArray()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = (int)(rnd.NextDouble() * 250);
+            }
+        }
+        public void OutputArray()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+        public void Sorts()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int g = i + 1; g < a.Length; g++)
+                {
+                    if (a[i] > a[g])
+                    {
+                        int t = a[i];
+                        a[i] = a[g];
+                        a[g] = t;
+                    }
+                }
+            }
+        }
+    }
+}
--- SortArray/Program.cs
+++ SortArray/Program.cs
@@ -8,48 +8,14 @@ namespace SortArray
 {
     class Program
     {
-        int[] a = new int[20];
-        public void generateArray()
-        {
-            Random rnd = new Random();
-            for (int i = 0; i < a.Length; i++)
-            {
-                a [i] = (int)(rnd.NextDouble () * 250);
-            }
-        }
-        public void outputArray()
-        {
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.Write(a [i] + " ");
-            }
-        }
-
-        public void sorts ()
-        {
-            for (int i = 0; i < a.Length; i++)
-            {
-                for (int g = i + 1; g < a.Length; g++)
-                {
-                    if(a [i] > a [g])
-                    {
-                        int t = a[i];
-                        a[i] = a[g];
-                        a[g] = t;
-                    }
-                }
-            }
-        }
-
         public static void Main()
         {
-          Program sort = new Program();
-
-             sort.generateArray();
-             sort.outputArray();
-             sort.sorts();
+            BubbleSort sort = new BubbleSort();
+            sort.GenerateArray();
+            sort.OutputArray();
+            sort.Sorts();
             Console.WriteLine();
-            sort.outputArray();
+            sort.OutputArray();
 
             Console.ReadKey();
          }
